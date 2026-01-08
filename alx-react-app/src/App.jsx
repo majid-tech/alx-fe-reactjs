481f3fc6d6e18fb509978a3af85bd3606065c25a
majid-tech <majidmohammed044@gmail.com>
2026-01-08 20:42:53 +0000

    Create UserProfile using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,12 +3,15 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
-
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
     <>
       <Header />
+      <UserProfile 
+        name="Alice" age="25" bio="Loves hiking and photography"
+      />
       <MainContent />
       <Footer />
     </>
