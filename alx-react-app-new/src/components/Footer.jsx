762cb6011c2f1b0d3342c60e8918d7c96699b99b
majid-tech <majidmohammed044@gmail.com>
2026-01-20 15:23:45 +0000

    duplicate app and style components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+        <>
+            <footer style={
+                {
+                    backgroundColor: 'darkblue', maxWidth: '100%', padding: '10px', color: 'whitesmoke'
+                }
+            }>
+                <p>&copy; 2023 City Lovers</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent() {
+    return (
+        <>
+            <main style={
+                {
+                    fontFamily: 'Arial', fontStyle: 'normal', color: 'Grey'
+                }
+            }
+            >
+                <p>I love to visit New York, Paris, and Tokyo.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate delectus quod iusto dolorum, odit beatae inventore rerum repellat, asperiores itaque molestias in! Optio aliquam voluptates reprehenderit? Nobis eos impedit quidem.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero qui odio sed hic accusantium, a animi illo voluptates eligendi quia et id aperiam, non fuga quis dolorem quas distinctio sunt?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, ratione officiis voluptatibus nulla ea, porro aliquid dicta nostrum doloremque sed provident? Consequatur voluptatem ipsam consequuntur numquam ab pariatur enim tempore?</p>
+            </main>
+        </>
+    )
+}
+
+export default MainContent;
