935b5b423da62ff273d18575584457419fc348a0
majid-tech <majidmohammed044@gmail.com>
2026-01-21 12:40:09 +0000

    create counter in react using useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,11 +4,13 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <>
       <Header />
+      <Counter />
       <UserProfile 
         name="Alice" age="25" bio="Loves hiking and photography"
       />
