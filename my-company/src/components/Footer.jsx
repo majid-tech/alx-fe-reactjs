53c6604d5eabe6737dbc7f79f03f10290b6ac096
majid-tech <majidmohammed044@gmail.com>
2026-01-26 11:41:41 +0000

    create company website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer
+      style={{
+        backgroundColor: "#222",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "40px"
+      }}
+    >
+      © 2026 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        display: "flex",
+        gap: "20px"
+      }}
+    >
+      <Link style={{ color: "white", textDecoration: "none" }} to="/">Home</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/about">About</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/services">Services</Link>
+      <Link style={{ color: "white", textDecoration: "none" }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
