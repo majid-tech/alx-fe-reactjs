d9efe9e362fc246581a82f9867b6578f2e6f52be
majid-tech <majidmohammed044@gmail.com>
2026-01-26 05:29:32 +0000

    create UserProfile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,24 +1,16 @@
-function UserProfile(props) {
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
+
+function UserProfile() {
+    const userData = useContext(UserContext)
+    
     return (
-        <div style={
-            {
-                border: '1px solid gray', padding:'10px', margin:'10px'
-            }
-        }>
-            <h2 style={
-                {
-                    color:'blue'
-                }
-            }
-            >{props.name}</h2>
-            <p>Age: <span style={
-                {
-                    fontWeight: 'bold'
-                }
-            }>{props.age}</span></p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 };
 
 export default UserProfile;
