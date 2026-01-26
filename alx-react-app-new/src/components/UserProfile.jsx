fd0b2decc7879ae101692d708cc87ce9c48bef28
majid-tech <majidmohammed044@gmail.com>
2026-01-26 06:00:17 +0000

    Add UserProfile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,16 +1,15 @@
-import { useContext } from "react";
-import UserContext from "./UserContext";
-
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 function UserProfile() {
-    const userData = useContext(UserContext)
-    
-    return (
+  const userData = useContext(UserContext);
+
+  return (
     <div>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
 export default UserProfile;
